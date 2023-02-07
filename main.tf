0f52ccce02884c0f6ff8d563ab398c06a5f8858a
jumpt57 <julienmaupetit@icloud.com>
2023-02-07 09:44:31 -0500

    Start working on infra

--- main.tf
+++ main.tf
@@ -20,6 +20,159 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# EC2
+
+resource "aws_instance" "instance_1" {
+  ami = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instances.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello world 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+resource "aws_instance" "instance_2" {
+  ami = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instances.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello world 2" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "default_subnet" {
+  vpc_id = data.aws_vpc.default_vpc.id
+}
+
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instances.id
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Load balancer
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "instances" {
+  name = "target-group"
+  port = 8080
+  protocol = "HTTP"
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance_1" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id = aws_instance.instance_1.id
+  port = 8080
+}
+
+resource "aws_lb_target_group_attachment" "instance_2" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id = aws_instance.instance_2.id
+  port = 8080
+}
+
+resource "aws_lb_listener_rule" "instances" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.instances.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "alb-security-group"
+}
+
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_alb_http_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb" "load_balancer" {
+  name = "web-app-lb"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default_subnet.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+# Route53
+
+resource "aws_route53_zone" "primary" {
+  name = "jumpt57.link"
+}
+
+resource "aws_route53_record" "root" {
+  name = "jumpt57.link"
+  type = "A"
+  zone_id = aws_route53_zone.primary.zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.load_balancer.dns_name
+    zone_id                = aws_lb.load_balancer.zone_id
+  }
+}
+
+
+# Terraform remote backend
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "jumpt57-terraform-sandbox-tf-state"
   force_destroy = true
